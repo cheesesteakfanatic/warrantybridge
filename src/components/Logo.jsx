62843271bdbe237f86aa17c9c917403d5025820c
cheesesteakfanatic <glenn.hubbard.career@gmail.com>
2026-06-11 11:31:00 -0400

    v2: settings, notifications+email, manager controls, metrics, password reset, redesign

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,10 @@
-export default function Logo({ onDark = false, size = 'md' }) {
+import { ILogoMark } from './Icons'
+
+export default function Logo({ onDark = false }) {
   return (
     <div className={`logo-row ${onDark ? 'on-dark' : ''}`}>
-      <span className="logo-mark">
-        <svg width="20" height="20" viewBox="0 0 32 32">
-          <path d="M16 5 4 15h4v11h7v-7h2v7h7V15h4L16 5z" fill="#5eead4" />
-        </svg>
-      </span>
-      WarrantyBridge
+      <span className="logo-mark"><ILogoMark size={20} /></span>
+      <span className="logo-word">Warranty<em>Bridge</em></span>
     </div>
   )
 }
